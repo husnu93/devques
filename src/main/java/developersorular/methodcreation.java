c6a49af304bbbb2d04c1dd817dd1157db3763a2c
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-01-06 18:22:53 +0300

    Update methodcreation.java

--- src/main/java/developersorular/methodcreation.java
+++ src/main/java/developersorular/methodcreation.java
@@ -1,15 +1,1 @@
-package developersorular;
 
-public class methodcreation {
-    public static void main(String[] args) {
-
-
-        System.out.println("cumle");
-
-    }
-
-
-   // public static void cumle(String"abc");
-
-
-}
