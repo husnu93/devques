c020132b550ab692d2f9380a130742ac71a8c4b8
husnu93 <149675198+husnu93@users.noreply.github.com>
2024-01-19 20:58:32 +0300

    Update faktoriyelalma.java

--- src/main/java/developersorular/faktoriyelalma.java
+++ src/main/java/developersorular/faktoriyelalma.java
@@ -8,11 +8,16 @@ public class faktoriyelalma {
         Scanner sayi = new Scanner(System.in);
         System.out.println("lütfen faktoriyel alınmasını istedigin sayiyi gir");
 
+
+
+        
         int sonuc = 1 ;
         int scan = sayi.nextInt();
         for (int i=1; i <= scan; i++) {
             sonuc = sonuc*i;
 
+
+            
         }
         System.out.println(sonuc);
 
